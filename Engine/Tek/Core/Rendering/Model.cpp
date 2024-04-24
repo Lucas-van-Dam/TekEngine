88f4f576b7510d22864f0ced61ecbd60f70db80f
Ciar <138235847+Lucas-van-Dam@users.noreply.github.com>
2024-04-24 13:01:46 +0200

    fixed texture not mapping correctly

--- Engine/Tek/Core/Rendering/Model.cpp
+++ Engine/Tek/Core/Rendering/Model.cpp
@@ -12,7 +12,7 @@ void Model::loadModel(string const &path)
     // read file via ASSIMP
     Assimp::Importer importer;
     importer.SetPropertyInteger("UpdateImporterScale", 100);
-    const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
+    const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_CalcTangentSpace);
     // check for errors
     if(!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) // if is Not Zero
     {
